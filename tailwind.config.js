b49ca1450f39d9c2799214560ab68b1db7fe764b
Tyler Povanda <tyler.povanda@gmail.com>
2026-03-12 19:21:03 -0400

    Extend custom theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "D#6CC6F",
+          200: "#B3E0FF",
+          300: "#80BFFF",
+          400: "#4DA6FF",
+          500: "#1A8CFF",
+        },
+        dark: {
+          100: "#1A1A1A",
+          200: "#333333",
+        },
+        accent: "#AB88FF",
+      },
+    },
   },
   plugins: [],
 };
