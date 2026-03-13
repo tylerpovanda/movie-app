c6c33bc0494fdbcad8eea8dec86acda81869dc1d
Tyler Povanda <tyler.povanda@gmail.com>
2026-03-12 21:23:40 -0400

    Begin adding/styling Home page, create search component.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,9 +9,9 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
-        <Image source={icon} tintColor="colors.secondary" className="size-5" />
+        <Image source={icon} tintColor="#030014" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
           {title}
         </Text>
@@ -20,7 +20,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
   } else {
     return (
       <View className="size-full justify-center items-center mt-4 rounded-full">
-        <Image source={icon} tintColor="colors.secondary" className="size-5" />
+        <Image source={icon} tintColor="#AB88FF" className="size-5" />
       </View>
     );
   }
---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>SearchBar</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
